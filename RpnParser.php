f588005a82532c402d48e32d29c5e45002c2e2a6
AVAsmakov <alexandr.asmakov@yandex.ru>
2019-10-13 18:55:17 +0300

    add exceptions in doc block

--- RpnParser.php
+++ RpnParser.php
@@ -3,6 +3,13 @@
 namespace App\Service\ExpressionParser;
 
 use App\Service\ExpressionParser\Calculator\RpnCalculator;
+use App\Service\ExpressionParser\Exceptions\EmptyExpressionException;
+use App\Service\ExpressionParser\Exceptions\IncorrectOperatorException;
+use App\Service\ExpressionParser\Exceptions\UseSpecialOperatorException;
+use App\Service\ExpressionParser\Exceptions\IncorrectExpressionBalanceException;
+use App\Service\ExpressionParser\Exceptions\DivisionByZeroException;
+use App\Service\ExpressionParser\Exceptions\EnoughDataOnTheStackException;
+use App\Service\ExpressionParser\Exceptions\NumberOfOperatorsException;
 
 class RpnParser extends AbstractParser
 {
@@ -22,6 +29,13 @@ class RpnParser extends AbstractParser
      * @return int|float
      *
      * @throws \Exception
+     * @throws EmptyExpressionException
+     * @throws UseSpecialOperatorException
+     * @throws IncorrectExpressionBalanceException
+     * @throws IncorrectOperatorException
+     * @throws DivisionByZeroException
+     * @throws NumberOfOperatorsException
+     * @throws EnoughDataOnTheStackException
      */
     public function parse(string $text)
     {
